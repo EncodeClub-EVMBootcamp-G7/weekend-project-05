26a373b5c9e5b4e9dd3ec69793303c9a8adf391f
Evangel90 <oladipoevangel90@gmail.com>
2024-11-30 19:38:44 +0100

    contract and scaffold-eth set

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -1,6 +1,7 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import { Contract } from "ethers";
+import { Contract, parseEther } from "ethers";
+import { parse } from "dotenv";
 
 /**
  * Deploys a contract named "YourContract" using the deployer account and
@@ -22,10 +23,16 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  await deploy("YourContract", {
+  await deploy("Lottery", {
     from: deployer,
     // Contract constructor arguments
-    args: [deployer],
+    args: [
+      "Group7LotteryToken",
+      "G7T",
+      10000n,
+      parseEther('0.0001'),
+      parseEther("0.00002")
+    ],
     log: true,
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
     // automatically mining the contract deployment transaction. There is no effect on live networks.
@@ -33,12 +40,13 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   });
 
   // Get the deployed contract to interact with it after deploying.
-  const yourContract = await hre.ethers.getContract<Contract>("YourContract", deployer);
-  console.log("👋 Initial greeting:", await yourContract.greeting());
+  const lotteryContract = await hre.ethers.getContract<Contract>("Lottery", deployer);
+  console.log("Lottery Contract address:", await lotteryContract.getAddress())
+  console.log("Lottery Token address:", await lotteryContract.paymentToken());
 };
 
 export default deployYourContract;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags YourContract
-deployYourContract.tags = ["YourContract"];
+deployYourContract.tags = ["Lottery"];
